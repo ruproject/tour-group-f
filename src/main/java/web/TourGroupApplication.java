67b8fa1085a391a70a565cf3172bc0e5e7142708
EasyIT <EasyIT@192.168.98.128>
2016-04-04 00:36:53 +0700

    master

---
+++ src/main/java/web/TourGroupApplication.java
@@ -0,0 +1,12 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourGroupApplication.class, args);
+	}
+}
